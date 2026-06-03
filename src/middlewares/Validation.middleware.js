131b5dd19b71eba23d21d38fdac12f011bfbf080
markyouhanna12 <markyouhanna1@gmail.com>
2026-06-04 01:42:51 +0300

    - implement validation schemas using Joi for user signup and login - Add generalFields

--- src/middlewares/Validation.middleware.js
+++ src/middlewares/Validation.middleware.js
@@ -1,4 +1,89 @@
+import { GenderEnum, ProviderEnum, RoleEnum } from "../utils/enums/user.enum.js";
 import { BadRequestException } from "../utils/response/error.response.js";
+import joi from "joi"
+
+export const generalFields = {
+    firstName : joi
+        .string()
+        .alphanum()
+        .min(2)
+        .max(25)
+        .messages({
+            "any.required":"First Name is required",
+            "string.empty":"First Name cannot be empty",
+            "string.alphanum":"First Name must contain only letters and numbers",
+            "string.min":"First Name must be at least 2 characters",
+            "string.max":"First Name must be at most 25 characters"
+        })
+        ,
+    
+         lastName : joi
+         .string()
+        .alphanum()
+        .min(2)
+        .max(25)
+        .messages({
+            "any.required":"Last Name is required",
+            "string.empty":"Last Name cannot be empty",
+            "string.alphanum":"Last Name must contain only letters and numbers",
+            "string.min":"Last Name must be at least 2 characters",
+            "string.max":"Last Name must be at most 25 characters"
+        })
+        ,
+    
+        email : joi
+        .string()
+        .email({
+            minDomainSegments: 1,
+            maxDomainSegments:3,
+            tlds:{allow:["com","net","org"]}
+        })
+        .messages({
+            "any.required":"Email is required",
+            "string.empty":"Email cannot be empty",
+            "string.email":"Invalid email format"
+        }),
+    
+    
+        age : joi
+        .number()
+        .integer()
+        .min(18)
+        .max(50),
+        isActive:joi.boolean()
+        .truthy("true","1",1,"Done","Yes")
+        .falsy("false","0",0,"Not Done","No"),
+    
+        password : joi
+        .string()
+        .min(8)
+        .max(25)
+        .pattern(new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$"))
+        .messages({
+            "any.required":"Password is required",
+            "string.empty":"Password cannot be empty",
+            "string.min":"Password must be at least 8 characters",
+            "string.max":"Password must be at most 25 characters",
+            "string.pattern.base":"Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character"
+        }),
+    
+        phone : joi
+        .string()
+        .pattern(new RegExp(/^01[0125]{1}[0-9]{8}$/)),
+
+        id: joi.string().custom((value, helpers) =>{
+
+            return ( Types.ObjectId.isValid(value) || helpers.message("Invalid ObjectId format") )
+        }),
+
+        gender: joi.string().valid(... Object.values(GenderEnum)),
+        role: joi.string().valid(... Object.values(RoleEnum)),
+        provider: joi.string().valid(... Object.values(ProviderEnum)),
+    
+}
+
+
+
 
 export const validation  = (schema) =>{
     return (req,res,next)=>{
--- src/modules/Auth/auth.validation.js
+++ src/modules/Auth/auth.validation.js
@@ -1,73 +1,21 @@
 import joi from "joi"
+import { generalFields } from "../../middlewares/Validation.middleware.js"
 
 
 export const signupSchema = {
     body: joi.object({
-    firstName : joi
-    .string()
-    .alphanum()
-    .min(2)
-    .max(25)
-    .messages({
-        "any.required":"First Name is required",
-        "string.empty":"First Name cannot be empty",
-        "string.alphanum":"First Name must contain only letters and numbers",
-    })
-    .required(),
 
-     lastName : joi
-     .string()
-    .alphanum()
-    .min(2)
-    .max(25)
-    .messages({
-        "any.required":"Last Name is required",
-        "string.empty":"Last Name cannot be empty",
-        "string.alphanum":"Last Name must contain only letters and numbers",
-    })
-    .required(),
+    firstName : generalFields.firstName.required(),
 
-    email : joi
-    .string()
-    .email({
-        minDomainSegments: 1,
-        maxDomainSegments:3,
-        tlds:{allow:["com","net","org"]}
-    })
-    .messages({
-        "any.required":"Email is required",
-        "string.empty":"Email cannot be empty",
-        "string.email":"Invalid email format"
-})
-    .required(),
+    lastName : generalFields.lastName.required(),
 
+    email : generalFields.email.required(),
 
-    age : joi
-    .number()
-    .integer()
-    .min(18)
-    .max(50),
-    isActive:joi.boolean()
-    .truthy("true","1",1,"Done","Yes")
-    .falsy("false","0",0,"Not Done","No"),
+    age : generalFields.age,
 
-    password : joi
-    .string()
-    .min(8)
-    .max(25)
-    .pattern(new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$"))
-    .messages({
-        "any.required":"Password is required",
-        "string.empty":"Password cannot be empty",
-        "string.min":"Password must be at least 8 characters",
-        "string.max":"Password must be at most 25 characters",
-        "string.pattern.base":"Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character"
-    })
-    .required(),
+    password : generalFields.password.required(),
 
-    phone : joi
-    .string()
-    .pattern(new RegExp(/^01[0125]{1}[0-9]{8}$/))
+    phone : generalFields.phone.required()
 
 })
 
@@ -79,33 +27,9 @@ export const signupSchema = {
 export const loginSchema = {
     body: joi.object({
 
-    email : joi
-    .string()
-    .email({
-        minDomainSegments: 1,
-        maxDomainSegments:3,
-        tlds:{allow:["com","net","org"]}
-    })
-    .messages({
-        "any.required":"Email is required",
-        "string.empty":"Email cannot be empty",
-        "string.email":"Invalid email format"
-})
-    .required(),
+    email : generalFields.email.required(),
 
-    password : joi
-    .string()
-    .min(8)
-    .max(25)
-    .pattern(new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$"))
-    .messages({
-        "any.required":"Password is required",
-        "string.empty":"Password cannot be empty",
-        "string.min":"Password must be at least 8 characters",
-        "string.max":"Password must be at most 25 characters",
-        "string.pattern.base":"Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character"
-    })
-    .required()
+    password : generalFields.password.required()
 
 })
 
